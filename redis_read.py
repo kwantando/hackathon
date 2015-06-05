6c2d31f48e7d9efdae32be4062dea080d44af61e
msimard8 <msimard8@gmail.com>
2015-06-05 17:01:34 -0400

    send client build status message if message has changed

--- redis_read.py
+++ redis_read.py
@@ -3,30 +3,34 @@ from constants import host_name
 import redis
 import pika
 import time
+import json
 
 r = redis.StrictRedis(host=host_name, port=6379, db=0)
-
-
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(host=host_name))
 channel = connection.channel()
 
-channel.exchange_declare(exchange='update',
+channel.exchange_declare(exchange='clientupdate',
                          type='fanout')
 
+saved_timestamp = 'nodata'
 
 while (1):
-	print 'checking for updates'
-	time.sleep(5)
-	print 'checking complete'
+	current_timestamp = r.hget("build","timestamp")
+	if (saved_timestamp != current_timestamp):
+		saved_timestamp = current_timestamp 
 
-
-
-message = ' '.join(sys.argv[1:]) or "info: Hello World!"
-channel.basic_publish(exchange='update',
+		data = {}
+		data['buildstatus'] = r.hget("build","buildstatus")
+		message = json.dumps(data)
+		channel.basic_publish(exchange='clientupdate',
                       routing_key='',
                       body=message)
-print " [x] Sent %r" % (message,)
+		print ' [*] sending ' + message
+ 	time.sleep(1)
+ 
+
+
 channel.close()
 connection.close()
 
